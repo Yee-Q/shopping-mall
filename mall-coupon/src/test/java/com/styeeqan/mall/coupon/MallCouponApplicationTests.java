feba2782193c94b8030260b0db7be6bccc26a1e9
Yee-Q <1051195623@qq.com>
2022-02-12 16:35:10 +0800

    项目初始结构创建

---
+++ mall-coupon/src/test/java/com/styeeqan/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.styeeqan.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
